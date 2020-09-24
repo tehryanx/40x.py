411ddee1cb69f2995d3e7bb77d90ffa74e9f834b
Ryan <ryan.cartner@gmail.com>
2020-09-24 14:01:54 -0300

    Fixed path bug

--- 40x.py
+++ 40x.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 
-import sys
+import sys,os
 
-pl_file = "./paths.txt"
+pl_file = os.path.abspath(os.path.dirname(__file__))+"/paths.txt"
 
 def read_in():
-	return [x.strip() for x in sys.stdin.readlines()]
+        return [x.strip() for x in sys.stdin.readlines()]
 
 urls = read_in()
 
 for url in urls:
-	if url == "":
-		continue
-	# remove trailing slash
-	url = url[:-1] if url[-1] == '/' else url
-	# target is the last element
-	target_path = url.split("/")[-1]
-	# url is everything but the final path element
-	url = "/".join(url.split("/")[:-1])
+        if url == "":
+                continue
+        # remove trailing slash
+        url = url[:-1] if url[-1] == '/' else url
+        # target is the last element
+        target_path = url.split("/")[-1]
+        # url is everything but the final path element
+        url = "/".join(url.split("/")[:-1])
 
 
-	with open(pl_file) as f:
-		payloads = f.readlines()
+        with open(pl_file) as f:
+                payloads = f.readlines()
 
-	payloads = [x.rstrip() for x in payloads] 
+        payloads = [x.rstrip() for x in payloads] 
 
-	for payload in payloads:
-		payload = payload.replace("TARGET", target_path)
+        for payload in payloads:
+                payload = payload.replace("TARGET", target_path)
 
-		print(f"{url}/{payload}")
+                print(f"{url}/{payload}")
